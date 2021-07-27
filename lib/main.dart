9b406204d5ae99761f5f665b4c959d8b939e6b78
mostafamoh176 <mostafamoh176@gmail.com>
2021-07-27 04:09:34 +0200

    messanger screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:login_screen/Login_Screen.dart';
+import 'package:login_screen/massenger_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: massengerScreen(),
     );
   }
+
 }
